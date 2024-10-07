a0cb5b130861953cc38e17292cd3ed3df3670e1b
Grzegorz Bednarek <gregorooooo02@gmail.com>
2024-10-07 20:22:51 +0200

    Added functionality to the lantern - you can now turn it on and off

---
+++ Assets/Scripts/LanternController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanternController : MonoBehaviour
+{
+    private GameObject lantern;
+
+    [Header("Functional Options")]
+    [SerializeField] private bool turnOnOffLantern = true;
+
+    [Header("Controls")]
+    [SerializeField] private KeyCode toggleLanternKey = KeyCode.F;
+
+    [Header("Materials")]
+    [SerializeField] private Material litMaterial;
+    [SerializeField] private Material unlitMaterial;
+
+    private void Awake() {
+        lantern = GameObject.Find("Lantern");
+    }
+
+    private void Update() {
+        if (turnOnOffLantern) {
+            HandleLanternLight();    
+        }
+    }
+
+    private void HandleLanternLight() {
+        if (Input.GetKeyDown(toggleLanternKey)) {
+            lantern.GetComponent<Light>().enabled = !lantern.GetComponent<Light>().enabled;
+
+            if (lantern.GetComponent<Light>().enabled) {
+                lantern.GetComponent<Renderer>().material = litMaterial;
+            } else {
+                lantern.GetComponent<Renderer>().material = unlitMaterial;
+            }
+        }
+    }
+}
