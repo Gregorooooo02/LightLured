74d7b5f7e7ef7432accd1fc64d659d62d8b27f2b
Grzegorz Bednarek <gregorooooo02@gmail.com>
2024-10-10 01:12:59 +0200

    Made an animal skull, which works with a lantern!

---
+++ Assets/Scripts/Items/SkullBehaviour.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkullBehaviour : MonoBehaviour
+{
+    [Header("Skull Components")]
+    [SerializeField] private GameObject meshRenderer;
+    [SerializeField] private Light skullLight;
+    [SerializeField] private ParticleSystem particles;
+
+    [Header("Target")]
+    [SerializeField] private Light lanternLight;
+    private bool isLanternOn;
+
+    private void Update() {
+        CheckLantern();
+    }
+
+    private void CheckLantern() {
+        isLanternOn = lanternLight.enabled;
+
+        if (isLanternOn) {
+            meshRenderer.layer = 30;
+            skullLight.enabled = true;
+
+            if (!particles.isPlaying)
+                particles.Play();
+        }
+        else {
+            meshRenderer.layer = 7;
+            skullLight.enabled = false;
+
+            if (particles.isPlaying)
+                particles.Stop();
+        }
+    }
+}
